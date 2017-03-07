d81fdff51e0bd4b4ce65ca6ba8d91ee8f1971ba8
Jarosław Cellary <jaroslaw@wikia-inc.com>
2017-03-07 10:02:52 +0100

    Update config.py
    Updated config template and added a comment

--- kapajira/config.py
+++ kapajira/config.py
@@ -5,5 +5,7 @@ JIRA_CONFIG = {
     "url":      '',
     "user":     '',
     "password": '',
-    "project":  ''
+    #List of Jira project names - kapajira looks for a component defined in each of them matching the service name
+    #and files the ticket in it. If no match is found, first project on the list is used.
+    "project":  []
 }
